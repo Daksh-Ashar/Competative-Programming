e47325f365442e90895e53aa2bfe03c64040f7d1
Daksh Ashar <44469751+Daksh-Ashar@users.noreply.github.com>
2021-01-31 12:29:45 +0530

    first commit

---
+++ Array/Print_Array.cpp
@@ -0,0 +1,17 @@
+/*Array Program*/
+#include<iostream>
+using namespace std;
+int main()
+{
+int i;
+int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+
+cout<<"Elements of array are:\n";
+for(i=0;i<10;i++)
+{
+	cout<<i<<" Element of the array is :"<<arr[i];
+	cout<<"\n";
+}
+
+
+}
